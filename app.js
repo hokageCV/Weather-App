71fe6e628445e824b6ecf14e3256d63c5348779a
hokageCV <chaitanyavaru@gmail.com>
2022-06-25 19:13:34 +0530

    import function

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-// https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=8f99876633dc56b8cd04a0c94c38f769
-
 import { renderData } from "./renderData.js";
-
 
 
 const form = document.querySelector('form');
