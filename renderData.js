341f34853f4db0a1be6180860532ce9a27040d41
Chaitanya Varu <98262685+hokageCV@users.noreply.github.com>
2022-06-26 17:00:08 +0530

    change language

--- renderData.js
+++ renderData.js
@@ -7,6 +7,7 @@ const fluffDiv = document.getElementById('fluffData');
 
 const cityName = document.createElement('h3');
 const weather = document.createElement('p');
+const weatherDesc = document.createElement('p');
 const temperature = document.createElement('p');
 const pressure = document.createElement('p');
 const humidity = document.createElement('p');
@@ -15,6 +16,7 @@ const latitude = document.createElement('p');
 
 cityDiv.appendChild(cityName);
 mainWeatherDiv.appendChild(weather);
+mainWeatherDiv.appendChild(weatherDesc);
 okayishDiv.appendChild(temperature);
 okayishDiv.appendChild(pressure);
 okayishDiv.appendChild(humidity);
@@ -25,10 +27,12 @@ fluffDiv.appendChild(latitude);
 
 async function renderData(e){
     const data = await dataLao(e);
+    let taapmaanInC = data.taapmaan +  -273.1 ; // kelvin to celsius
 
     cityName.textContent = `${data.shahar}`;
     weather.textContent = `Weather : ${data.mausam}`;
-    temperature.textContent = `Temperature : ${data.taapmaan}`;
+    weatherDesc.textContent = `मौसम : ${data.mausamDesc}`;
+    temperature.textContent = `Temperature : ${taapmaanInC.toFixed(2)}℃ `; 
     pressure.textContent = `Pressure : ${data.vayuMandalDabav}`;
     humidity.textContent = `Humidity : ${data.namee}`;
     longitude.textContent = `Latitude : ${data.longitude}`;
